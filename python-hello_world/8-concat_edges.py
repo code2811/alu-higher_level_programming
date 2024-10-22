4543f1878bb99764c77bf49be2e663c3a80593bd
code2811 <f.kayiranga@alustudent.com>
2024-10-22 05:04:30 -0700

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-str1 = "object-oriented"
-str2 = "programming"
-str3 = "with"
-str4 = "Python"
-print(f"{str1} {str2} {str3} {str4}")
+print(" ".join(["object-oriented", "programming", "with", "Python"]))
 
