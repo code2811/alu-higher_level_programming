38fcd429ab20b65c4df4f84d9773d1f2af1bada9
code2811 <f.kayiranga@alustudent.com>
2024-10-24 11:54:20 -0700

    0-print_list_integer.py

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """
+    Print all integers of a list.
+    Args:
+        my_list: list of integers (default empty list)
+    Raises:
+        TypeError: if an element in the list is not an integer
+    """
     for number in my_list:
+        if not isinstance(number, int):
+            raise TypeError("list contains non-integer elements")
         print("{:d}".format(number))
