a4913d70b8dbe619a3a24e89bca81f7a2b65656a
code2811 <f.kayiranga@alustudent.com>
2024-10-24 12:15:22 -0700

    4-new_in_list.py

--- 4-new_in_list.py
+++ 4-new_in_list.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Replace element at specific position in a copy of the list.
-
-    Args:
-        my_list: original list
-        idx: index to replace element
-        element: new element to insert
-
-    Returns:
-        Modified copy if index valid, copy of original list otherwise
-    """
-    list_copy = my_list[:]
-    if idx < 0 or idx >= len(my_list):
-        return list_copy
-    list_copy[idx] = element
-    return list_copy
+    if my_list:
+        if idx < 0 or idx >= len(my_list):
+            return my_list[:]
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
+    return my_list
