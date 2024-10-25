6b30fde598575cc2c02e0c11bf866a936ba14908
code2811 <f.kayiranga@alustudent.com>
2024-10-25 02:28:37 -0700

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def multiple_returns(sentence):
-    if not sentence:
-        return (0, None)
-    return (len(sentence), sentence[0])
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    b = tuple_b + (0, 0)[:2 - len(tuple_b)]
+    return (a[0] + b[0], a[1] + b[1])
