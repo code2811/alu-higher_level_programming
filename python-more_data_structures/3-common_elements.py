c60af0b314320addcde4c3627be85c783281ecee
code2811 <f.kayiranga@alustudent.com>
2024-10-25 12:22:17 -0700

    3-common_elements.py

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets
+    Args:
+        set_1: first set
+        set_2: second set
+    Returns:
+        Set of elements present in both sets
+    """
+    return set_1 & set_2
