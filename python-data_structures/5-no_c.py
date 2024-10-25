5725d6f4384f876908c2c9a80b7bf1d3882d5a54
code2811 <f.kayiranga@alustudent.com>
2024-10-25 07:00:27 -0700

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    new_string = ''
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
-    return new_string
+    return ''.join(char for char in my_string if char not in 'cC')
