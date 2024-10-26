46cb09825c89457de5a282fff40c60a24af3bc6d
code2811 <f.kayiranga@alustudent.com>
2024-10-26 06:12:30 -0700

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Create a new matrix with the same size using list comprehension
-    return [[x ** 2 for x in row] for row in matrix]
+    """
+    Computes the square value of all integers of a matrix.
+    
+    Args:
+        matrix: A 2 dimensional array of integers
+        
+    Returns:
+        A new matrix with all values squared
+    """
+    return [[x**2 for x in row] for row in matrix]
