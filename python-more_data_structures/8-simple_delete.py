5db5209ac1c06b056d5e5aa2e89764aa47216b32
code2811 <f.kayiranga@alustudent.com>
2024-10-27 07:11:17 -0700

    8-simple_delete.py

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    """
-    Deletes a key in a dictionary
-    Args:
-        a_dictionary: input dictionary
-        key: key to delete (always a string)
-    Returns:
-        modified dictionary
-    """
-    if key in a_dictionary:
-        del a_dictionary[key]
-    return a_dictionary
+   if key in a_dictionary:
+       del a_dictionary[key]
+   return a_dictionary
