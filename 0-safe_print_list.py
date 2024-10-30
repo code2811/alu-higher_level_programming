f5689a348b3cac867cd40b0e49d3c664723b5a7b
code2811 <f.kayiranga@alustudent.com>
2024-10-30 05:21:59 -0700

    0-safe_print_list.py

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
@@ -8,5 +7,5 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         pass
     finally:
-        print("")
+        print()
     return count
