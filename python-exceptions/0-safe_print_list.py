0886188a82ac1d801148afc3a19f3429fb167c50
code2811 <f.kayiranga@alustudent.com>
2024-11-03 05:23:35 -0800

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,13 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """
+    Prints x elements of a list and returns number of elements printed
+    Args:
+        my_list: list of elements
+        x: number of elements to print
+    Returns:
+        The real number of elements printed
+    """
     count = 0
     try:
         for i in range(x):
@@ -8,4 +17,7 @@ def safe_print_list(my_list=[], x=0):
         pass
     finally:
         print("")
-    return count
+    return coun
+
+
+
