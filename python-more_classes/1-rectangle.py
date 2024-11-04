d50c47bc0d55e2f49041dcec173d5cb0e17ca37a
code <f.kayiranga@alustudent.com>
2024-11-04 09:19:10 -0800

    1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,42 +1,70 @@
-#!/usr/bin/python3
-"""
-This module defines a Rectangle class with width and height properties.
-"""
+try:
+    my_rectangle = Rectangle(2, -3)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
+try:
+    my_rectangle = Rectangle(-2, 3)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
-class Rectangle:
-    """Defines a rectangle with width and height properties."""
+try:
+    my_rectangle = Rectangle(2, 3)
+    my_rectangle.width = -4
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
-    def __init__(self, width=0, height=0):
-        """Initialize the rectangle with optional width and height."""
-        self.width = width
-        self.height = height
+try:
+    my_rectangle = Rectangle(2, 3)
+    my_rectangle.width = "4"
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
+try:
+    my_rectangle = Rectangle(2, 3)
+    my_rectangle.height = -4
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
-    def width(self):
-        """Retrieve the width of the rectangle."""
-        return self.__width
+try:
+    my_rectangle = Rectangle(2, 3)
+    my_rectangle.height = "4"
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
+try:
+    my_rectangle = Rectangle(2, "3")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
-    def width(self, value):
-        """Set the width of the rectangle, with validation."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+try:
+    my_rectangle = Rectangle("2", 3)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
+# Other expected output cases
+my_rectangle = Rectangle(2, 4)
+print(sorted(my_rectangle.__dict__))
 
-    def height(self):
-        """Retrieve the height of the rectangle."""
-        return self.__height
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.width)
 
-    
-    def height(self, value):
-        """Set the height of the rectangle, with validation."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.height)
+
+my_rectangle = Rectangle(4)
+print("{} - {}".format(my_rectangle.width, my_rectangle.height))
+
+my_rectangle = Rectangle()
+print("{} - {}".format(my_rectangle.width, my_rectangle.height))
+
+my_rectangle = Rectangle(2, 4)
+print("{} - {}".format(my_rectangle.width, my_rectangle.height))
+my_rectangle.width = 10
+print("{} - {}".format(my_rectangle.width, my_rectangle.height))
+
+my_rectangle = Rectangle(2, 4)
+print("{} - {}".format(my_rectangle.width, my_rectangle.height))
+my_rectangle.height = 10
+print("{} - {}".format(my_rectangle.width, my_rectangle.height))
 
