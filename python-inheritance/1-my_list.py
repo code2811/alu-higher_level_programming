f9526f18fc7834696eab07d62d80d72219f6abd6
code <f.kayiranga@alustudent.com>
2024-11-06 12:33:02 -0800

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,7 @@
-lass MyList(list):
-    """A list that can print itself in sorted order."""
-    
-    def print_sorted(self):
-        """Print the list in ascending sorted order."""
-        print(sorted(self))
+class MyList(list):
+         """A list that can print itself in sorted order."""
+         
+         def print_sorted(self):
+             """Print the list in ascending sorted order."""
+             print(sorted(self))
+     
