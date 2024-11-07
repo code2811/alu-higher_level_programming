11a54bdf45f23a3c54760cd841be03817acf91a6
code2811 <f.kayiranga@alustudent.com>
2024-11-07 03:13:02 -0800

    10-square.py

---
+++ 1-my_list.py/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define a Square class that inherits from Rectangle."""
+from rectangle import Rectangle
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+
