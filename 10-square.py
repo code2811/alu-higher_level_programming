221c071eb9b18c3a594ff22814483846d4ed77e6
code2811 <f.kayiranga@alustudent.com>
2024-11-07 14:03:54 -0800

    [Add] Square class inheriting from Rectangle

---
+++ 10-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Module for Square class that inherits from Rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class representing a square, inheriting from Rectangle.
+    This class defines a square by its size and implements
+    area calculation.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a Square instance.
+
+        Args:
+            size (int): The size of the square sides
+        Raises:
+            TypeError: If size is not an integer
+            ValueError: If size is less than or equal to 0
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate and return the area of the square.
+
+        Returns:
+            int: The area of the square
+        """
+        return self.__size ** 2
+
+Version 3 of 3
+
+
+
+
