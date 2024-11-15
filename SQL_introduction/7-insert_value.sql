363d4f60951d65be7cd7555ea9ae3e6e147dc08b
code2811 <f.kayiranga@alustudent.com>
2024-11-15 12:26:03 -0800

    7-insert_value.sql

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,7 @@
+-- This script inserts a new row into the table 'first_table' in the current database.
+-- New row details:
+--   id = 89
+--   name = 'Best School'
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+
