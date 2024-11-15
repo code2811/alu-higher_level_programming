1943642e0c54478c5384311a4e276216b497e708
code2811 <f.kayiranga@alustudent.com>
2024-11-15 07:51:28 -0800

    3-list_tables.sql

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,6 @@
 -- List all tables in the 'mysql' database;
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+USE hbtn_0c_0;
+CREATE TABLE first_table (id INT, name VARCHAR(256));
+CREATE TABLE second_table (id INT, name VARCHAR(256));
+CREATE TABLE third_table (id INT, name VARCHAR(256));
