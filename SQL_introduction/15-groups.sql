918cd8aa52dc36239084beff8cef2acc312f73e2
code2811 <f.kayiranga@alustudent.com>
2024-11-15 13:18:27 -0800

    15-groups.sql

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,9 @@
-Lists the number of records for each score in second_table
-SELECT score, COUNT(*) as number
+-- This script lists the number of records with the same score in the table 'second_table'.
+-- Results display 'score' and the count of records for each score, labeled as 'number'.
+-- The list is sorted by the count of records in descending order.
+
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
 ORDER BY number DESC;
+
