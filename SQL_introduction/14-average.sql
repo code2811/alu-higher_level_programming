7c8e78ffc8842eaa0c13b0332b78214292fb1fef
code2811 <f.kayiranga@alustudent.com>
2024-11-15 13:21:02 -0800

    14-average.sql

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- This script computes the average score of all records in the table 'second_table'.
+-- The result column is labeled as 'average'.
+
+SELECT AVG(score) AS average
+FROM second_table;
+
