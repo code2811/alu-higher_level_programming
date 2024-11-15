70c68f4ea02eadad74608a925fdd5ccc1018d3ba
code2811 <f.kayiranga@alustudent.com>
2024-11-15 08:12:28 -0800

    5-full_table.sql

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- This script prints the full description of the table 'first_table' in the database.
+-- It uses the `SHOW CREATE TABLE` statement to display the table structure.
+
+SHOW CREATE TABLE first_table;
+
