2a51d7fd42ccf03697fc5335bd9b195803a6c733
code2811 <f.kayiranga@alustudent.com>
2024-11-16 08:33:58 -0800

    16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,10 +1,9 @@
--- Ensure you are using the correct database
-USE hbtn_0d_tvshows;
-
--- List shows and their linked genres
+-- A cript that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+-- Query cript that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
-
+     LEFT JOIN tv_show_genres
+     	  ON tv_show_genres.show_id = tv_shows.id
+     LEFT JOIN tv_genres
+     	  ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
