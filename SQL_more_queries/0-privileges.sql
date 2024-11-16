0f8652bcde6ed2a5a87ae89843d2c1bb982f639d
code2811 <f.kayiranga@alustudent.com>
2024-11-16 06:22:38 -0800

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,6 @@
--- Script to show privileges for MySQL users user_0d_1 and user_0d_2
-
--- Show privileges for user_0d_1
+-- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Show privileges for user_0d_2
+-- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
