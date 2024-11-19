eea6c0fc6d834814ea2d82162c2ba54cb6f9a881
code <f.kayiranga@alustudent.com>
2024-11-19 14:36:47 +0000

    0-body_size.sh

--- alu-higher_level_programming/0-body_size.sh
+++ alu-higher_level_programming/0-body_size.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-# A script to send a request to a URL and display the size of the body in bytes.
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a request to the URL and display the size of the body in bytes
+# Displays the size of the body of the response from a given URL in bytes
 curl -s -o /dev/null -w "%{size_download}\n" "$1"
 
