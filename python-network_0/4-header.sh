0ebd5f22e057e4565fe907eff1c4c308137f055b
code <f.kayiranga@alustudent.com>
2024-11-19 17:17:52 +0000

    4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Sends a GET request to a URL with a custom header and displays the response body
+# Sends a GET request with a custom header and displays the response body
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+
 
