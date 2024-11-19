02c30281d7b36cbf41814ea60fbee435bca99a40
code <f.kayiranga@alustudent.com>
2024-11-19 17:24:55 +0000

    4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to send GET request with custom X-HolbertonSchool-User-Id header
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Script to send GET request with custom header and validate route
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1" -w "%{http_code}"
