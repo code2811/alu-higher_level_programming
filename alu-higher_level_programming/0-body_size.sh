2e1a438d73697f03ed42fe069f4be17edf08887d
code <f.kayiranga@alustudent.com>
2024-11-19 14:32:26 +0000

    0-body_size.sh

--- alu-higher_level_programming/0-body_size.sh
+++ alu-higher_level_programming/0-body_size.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-# Script to retrieve the size of the body response from a given URL
-curl -sI "$1" | grep -i "Content-Length:" | awk '{print $2}'
+# A script to send a request to a URL and display the size of the body in bytes.
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a request to the URL and display the size of the body in bytes
+curl -s -o /dev/null -w "%{size_download}\n" "$1"
+
