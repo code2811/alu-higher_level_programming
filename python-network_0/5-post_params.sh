657b7ad57862eee63347f7dbfd93b12d85fe16f2
code <f.kayiranga@alustudent.com>
2024-11-20 10:42:49 +0000

    5-post_params.sh

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to send POST request with email and subject parameters
-curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"i
+# Script that sends a POST request with email and subject parameters
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"i
