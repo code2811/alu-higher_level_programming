352d60b1a895680d385560c79caa621d6664d911
code2811 <f.kayiranga@alustudent.com>
2024-11-22 14:30:34 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""
-Module to fetch and display status from a given URL.
-
-This script uses urllib to retrieve the status of a specified URL
-and prints details about the response body.
-"""
+"""Module to fetch status from a given URL."""
 import urllib.request
 
-def fetch_status(url='https://intranet.hbtn.io/status'):
-    """
-    Fetch status from the given URL and display response details.
-
-    Args:
-        url (str): URL to fetch status from. 
-                   Defaults to 'https://intranet.hbtn.io/status'
-    """
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode('utf-8')))
+def fetch_status():
+    """Fetch and display status from the specified URL."""
+    url = 'https://intranet.hbtn.io/status'
+    
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode('utf-8')))
+    except Exception as e:
+        print("Error fetching status:", e)
 
 if __name__ == "__main__":
     fetch_status()i
