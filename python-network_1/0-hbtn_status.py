04ff1a3a23108da4ad2f435d238284486e78c94f
code2811 <f.kayiranga@alustudent.com>
2024-11-22 14:17:56 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,7 +1,15 @@
+#!/usr/bin/python3
+"""
+Module to fetch status from a given URL using urllib.
+Displays response details as specified.
+"""
 import urllib.request
 
 def fetch_status():
-    url = 'https://alu-intranet.hbtn.io/status'
+    """
+    Fetch and display status from the specified URL.
+    """
+    url = 'https://intranet.hbtn.io/status'
     
     with urllib.request.urlopen(url) as response:
         body = response.read()
