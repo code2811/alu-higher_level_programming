12f7ddcf10a92eaac3049a10d4ada5885fc8bbc6
f.kayiranga@alustudent.com <code2811>
2024-11-23 21:40:34 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://alu-intranet.hbtn.io/status
+Python script that fetches http://0.0.0.0:5050/status
 and displays the response body with specific formatting
 """
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://alu-intranet.hbtn.io/status")
+    r = requests.get("http://0.0.0.0:5050/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
