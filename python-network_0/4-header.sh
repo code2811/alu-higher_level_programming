a5bf4fb3621d3aa6d397718f9182fd4879f78678
code2811 <f.kayiranga@alustudent.com>
2024-11-23 13:47:43 +0000

    4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a GET request with header X-HolbertonSchool-User-Id: 98
+# Script that sends a GET request to validate route with header parameter
 curl -sH "X-HolbertonSchool-User-Id: 98" "$1"i
