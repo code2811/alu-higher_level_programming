af6d9bebd9affe14bf6eb1adf0a7dfc7f73e1fbf
f.kayiranga@alustudent.com <code2811>
2024-11-23 15:02:42 +0000

    What's my status

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""
-A script that fetches the content of a given URL using urllib.
+"""A Python script that fetches a status URL.
 
-The body of the response is displayed with:
-    - type: the type of the response content
-    - content: the raw content (bytes)
-    - utf8 content: the decoded content (string)
+This script uses urllib package to make a request to a URL
+and displays information about the response including its
+type, content in bytes, and UTF-8 decoded content.
 """
+from urllib import request
 
-import urllib.request
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5050/status"  # You can replace this with any URL.
-
-    with urllib.request.urlopen(url) as response:
+    url = 'https://intranet.hbtn.io/status'
+    with request.urlopen(url) as response:
         content = response.read()
         print("Body response:")
-        print(f"\t- type: {type(content)}")
-        print(f"\t- content: {content}")
-        print(f"\t- utf8 content: {content.decode('utf-8')}")
-i
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))i
