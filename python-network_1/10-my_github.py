404af2f2478e49b4c649b3b5d49e7a789490fe26
f.kayiranga@alustudent.com <code2811>
2024-11-23 20:46:49 +0000

    10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+Python script that takes GitHub credentials (username and password)
+and uses the GitHub API to display your id
+Username and token are given as arguments
+"""
 import requests
 import sys
 
@@ -6,14 +12,10 @@ if __name__ == "__main__":
     username = sys.argv[1]
     token = sys.argv[2]
 
-    # GitHub API endpoint for user information
     url = "https://api.github.com/user"
-
-    # Create Basic Authentication header using username and token
     response = requests.get(url, auth=(username, token))
 
-    # Check if authentication was successful and print user ID
     if response.status_code == 200:
         print(response.json().get('id'))
     else:
-        print(None)i
+        print(None)
